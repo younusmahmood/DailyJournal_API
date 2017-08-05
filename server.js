3ac182f0b5243c0f58f37af9e8dcdeba2633ed1f
younusmahmood <younus23@gmail.com>
2017-08-04 20:42:23 -0500

    Setup separate test and development envs

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('./config');
+
 const _ = require('lodash')
 const express = require('express');
 const bodyParser = require("body-parser");
@@ -9,7 +11,7 @@ var { mongoose } = require('./db/mongoose');
 var { TaskList } = require('./models/taskList');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(cors())
