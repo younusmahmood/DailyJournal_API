49fb67c93e1f7746df54de57e442b3a7801cf1d7
younusmahmood <younus23@gmail.com>
2017-08-28 12:55:02 -0500

    Added POST users route and token method

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { ObjectID } = require('mongodb');
-const fs = require('fs');
 
 var { mongoose } = require('./db/mongoose');
 var { TaskList } = require('./models/taskList');
+var { User } = require('./models/user')
 
 var app = express();
 const port = process.env.PORT;
@@ -80,6 +80,20 @@ app.delete('/taskslist/:id', (req, res) => {
     }).catch((e) => res.status(404).send());
 
 });
+
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body)
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+
+})
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
