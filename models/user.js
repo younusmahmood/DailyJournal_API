dd84e471db31cff280214f8447d6658c00d8b908
younusmahmood <younus23@gmail.com>
2017-08-28 19:21:50 -0500

    Hash user password before saving

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const _ = require('lodash');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
-
+const bcrypt =  require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -69,6 +69,24 @@ UserSchema.statics.findByToken = function (token) {
         'tokens.access': 'auth'
     })
 }
+
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+    if(user.isModified('password')){
+        var password = user.password;
+
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+    
+}) 
 
 var User = mongoose.model("User", UserSchema);
 
