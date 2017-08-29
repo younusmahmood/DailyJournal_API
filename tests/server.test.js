540494c8a815c5e593c9780c5c011eacf193e3d8
younusmahmood <younus23@gmail.com>
2017-08-29 09:44:39 -0500

    add test for login route

--- tests/server.test.js
+++ tests/server.test.js
@@ -209,3 +209,62 @@ describe('POST /users', () => {
             .end(done)
     });
 })
+
+describe('POST /users/login', () => {
+    it('Should login user and return auth token', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: users[1].password
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toExist()
+            })
+            .end((err, res) => {
+                if(err) {
+                    return done(err)
+                }
+
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens[0]).toInclude({
+                        access: 'auth',
+                        token: res.headers['x-auth']
+                    })
+                    done();
+                }).catch(e => {
+                    done(e)
+                })
+            })
+    });
+
+    it('Should reject invalid login', (done) => {
+        request(app)
+            request(app)
+              .post("/users/login")
+              .send({
+                email: users[1].email,
+                password: '1111'
+              })
+              .expect(400)
+              .expect(res => {
+                expect(res.headers["x-auth"]).toNotExist();
+              })
+              .end((err, res) => {
+                if (err) {
+                  return done(err);
+                }
+
+                User.findById(users[1]._id)
+                  .then(user => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                  })
+                  .catch(e => {
+                    done(e);
+                  });
+              });
+
+    })
+})
